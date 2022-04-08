76c096a88e0f534397aed62ab68a607871d89e83
eemilyl <eevett7@gmail.com>
2022-04-08 00:46:22 -0600

    Details details

--- Startup.cs
+++ Startup.cs
@@ -89,7 +89,7 @@ namespace intex2
 
 
             services.AddSingleton<InferenceSession>(
-                new InferenceSession("intex_crash.onnx"));
+                new InferenceSession("wwwroot/intex_crash.onnx"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
